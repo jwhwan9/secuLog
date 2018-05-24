c8fc2b7ab93c66c77f83d94fda60fb502a532ff5
jwhwan9 <jwhwang@gmail.com>
2018-05-24 11:43:36 +0800

    Add files via upload

--- LogGathering/GetLog.cmd
+++ LogGathering/GetLog.cmd
@@ -1,10 +1,109 @@
-@echo off
+﻿@echo off
 
 REM set logFolder=IR.%date:~-15,4%-%date:~-8,2%%date:~-5,2%-%time:~-11,2%%time:~-8,2%.%computername%
 set logFolder=IR.%date:~-15,4%-%date:~-8,2%%date:~-5,2%.%computername%
 del /f/s %logFolder%
 md %logFolder%
 md %logFolder%\eventlog
+
+@REM Netbios Name
+nbtstat -c >  %logFolder%\NetBiosCache.txt
+nbtstat -s >  %logFolder%\NetBiosSession.txt
+nbtstat -n >  %logFolder%\NetBiosName.txt
+
+@REM Domain User Account Information
+echo --net group /domain-- > %logFolder%\NetDomainAccount.txt
+net group /domain >>  %logFolder%\NetDomainAccount.txt
+
+echo --net user /domain-- >> %logFolder%\NetDomainAccount.txt
+net user /domain >>  %logFolder%\NetDomainAccount.txt
+
+echo --net group "Domain Admins" /DOMAIN-- >> %logFolder%\NetDomainAccount.txt
+net group "Domain Admins" /DOMAIN >>  %logFolder%\NetDomainAccount.txt
+
+echo --net accounts /domain-- >> %logFolder%\NetDomainAccount.txt
+net accounts /domain > %logFolder%\NetDomainAccount.txt
+
+echo --net group "Domain Controllers" /domain-- >> %logFolder%\NetDomainAccount.txt
+net group "Domain Controllers" /domain >> %logFolder%\NetDomainAccount.txt
+
+@REM Local User Account Information
+echo --net user-- > %logFolder%\NetAccount.txt
+net user >> %logFolder%\NetAccount.txt
+
+echo --net localgroup administrators-- >> %logFolder%\NetAccount.txt
+net localgroup administrators >> %logFolder%\NetAccount.txt
+
+echo --net accounts-- >> %logFolder%\NetAccount.txt
+net accounts >> %logFolder%\NetAccount.txt
+
+echo --net user securityadmin-- >> %logFolder%\NetAccount.txt
+net user securityadmin >> %logFolder%\NetAccount.txt
+
+@rem Dump Netbios based on Current IP/Class B
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+start /B nbtscan %NetworkIP%/16 > %logFolder%\NetBios_Dump_%NetworkIP%.txt
+
+@rem Export Autoruns
+Autorunsc.exe /accepteula -a * -c -h -s '*' > %logFolder%\Autoruns.csv
+
+@rem Dump Netbios based on Current IP/Class B full
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+start /B nbtscan -f %NetworkIP%/16 > %logFolder%\NetBios_Dump_%NetworkIP%_Full.txt
+
+@rem net use share folder
+net use > %logFolder%\netUseShare.txt
+
+@REM Domain User Group
+net group /domain > %logFolder%\netDomainGroup.txt
+
+
+@REM File Create Information
+dir C:\ProgramData /S /O-D /tc /q > %logFolder%\Dir_TC_ProgramData.txt
+start /B dir C:\Users /S /O-D /tc /q > %logFolder%\Dir_TC_Users.txt
+dir "C:\Program Files" /S /O-D /tc /q > %logFolder%\Dir_TC_ProgramFiles.txt
+dir "C:\Program Files (x86)" /S /O-D /tc /q > %logFolder%\Dir_TC_ProgramFilesx86.txt
+start /B dir C:\Windows /S /O-D /tc /q > %logFolder%\Dir_TC_Windows.txt
+dir C:\Temp /S /O-D /tc /q > %logFolder%\Dir_TC_Temp.txt
+start /B dir C:\ /O-D /tc /q > %logFolder%\Dir_TC_C.txt
+dir C:\ /AH /O-D /tc /q > %logFolder%\Dir_TC_Hidden.txt
+
+@REM File Modify Information
+dir C:\ProgramData /S /O-D /tw /q > %logFolder%\Dir_TW_ProgramData.txt
+start /B dir C:\Users /S /O-D /tw /q > %logFolder%\Dir_TW_Users.txt
+dir "C:\Program Files" /S /O-D /tw /q > %logFolder%\Dir_TW_ProgramFiles.txt
+dir "C:\Program Files (x86)" /S /O-D /tw /q > %logFolder%\Dir_TW_ProgramFilesx86.txt
+start /B dir C:\Windows /S /O-D /tw /q > %logFolder%\Dir_TW_Windows.txt
+dir C:\Temp /S /O-D /tw /q > %logFolder%\Dir_TW_Temp.txt
+dir C:\ /O-D /tw /q > %logFolder%\Dir_TW_C.txt
+start /B dir C:\ /AH /O-D /tw /q > %logFolder%\Dir_TW_Hidden.txt
+
+@REM Dump GPO Result
+gpresult /x %logFolder%\gpresult.xml
+gpresult /h %logFolder%\gpresult.html
+gpresult /z %logFolder%\gpresultZ.txt
+
+@REM System Information
+systeminfo > %logFolder%\Systeminfo.txt
+
+@REM Get Unsigned EXE，DLL in c:\windows
+sigcheck.exe -c -s -u c:\windows\*.exe > %logFolder%\WindowsUnsignedEXE.txt
+sigcheck.exe -c -s -u c:\windows\*.dll > %logFolder%\WindowsUnsignedDLL.txt
+
+@REM MSFT Native Backdoor Check
+echo %date% %time% > %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*sethc.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*Utilman.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*osk.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*Narrator.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*Magnify.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*AtBroker.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*explorer.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*displayswitch.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*cmd.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+
+@REM Net User Account Information
+net user %UserName% /domain > %logFolder%\Account_%UserName%_Info.txt
 
 @REM Get All User Account
 net view > %logFolder%\dcHost.txt
@@ -15,9 +114,19 @@ cscript GetGroup.vbs > %logFolder%\dcGroupOU.txt
 cscript GetUser.vbs > %logFolder%\dcUserOU.txt
 cscript GetContact.vbs > %logFolder%\dcContactOU.txt
 
+@REM Netbios Name
+nbtstat -c >  %logFolder%\NetBiosStat.txt
+
 @REM Windows Current Service
 tasklist /v > %logFolder%\tasklistVerbose.txt
 tasklist > %logFolder%\tasklistVerbose.txt
+tasklist /SVC > %logFolder%\taskSVC.txt
+
+@REM Schedule Job
+schtasks /query /v /fo CSV > %logFolder%\scheduleTasks.csv
+
+@REM Web Visit Log
+Weblog /scomma %logFolder%\weblog.csv /sort "Visit Time"
 
 @REM DNS Cache
 ipconfig /displaydns > %logFolder%\DisplayDNS.txt
@@ -38,9 +147,11 @@ wmic /Output:"%logFolder%\nicconfig.csv"  nicconfig list full /Format:CSV
 wmic /Output:"%logFolder%\nic.csv"  nic list full /Format:CSV
 wmic /Output:"%logFolder%\netuse.csv"  netuse list full /Format:CSV
 wmic /Output:"%logFolder%\netprotocol.csv"  netprotocol list full /Format:CSV
+wmic /Output:"%logFolder%\InstalledAppList.csv"  product list full /Format:CSV
 
 @REM DNS Cache
-netstat -nba > %logFolder%NetstatAP.txt
+netstat -nba > %logFolder%\NetstatAP.txt
+netstat -fba > %logFolder%\NetstatFQDN.txt
 
 @REM GET MFT
 rawcopy64 %SYSTEMDRIVE%0 %logFolder%
@@ -56,8 +167,11 @@ rawcopy64 %SystemRoot%\system32\config\DEFAULT %logFolder%
 rawcopy64 %SystemRoot%\system32\config\SECURITY %logFolder%
 rawcopy64 %SystemRoot%\system32\config\SOFTWARE %logFolder%
 
+rawcopy64  %SystemRoot%\AppCompat\Programs\RecentFileCache.bcf %logFolder%
+
 rawcopy64 %LOCALAPPDATA%\Microsoft\Windows\UsrClass.dat %logFolder%
 
+AppCompatCacheParser.exe  -s %logFolder% -t
 
 @REM Get Event Log
 copy %SystemRoot%\System32\winevt\Logs\* %logFolder%\eventlog
--- LogGathering/GetLogLite.cmd
+++ LogGathering/GetLogLite.cmd
@@ -1,4 +1,4 @@
-@echo off
+﻿@echo off
 
 REM set logFolder=IR.%date:~-15,4%-%date:~-8,2%%date:~-5,2%-%time:~-11,2%%time:~-8,2%.%computername%
 set logFolder=IR.%date:~-15,4%-%date:~-8,2%%date:~-5,2%.%computername%
@@ -6,12 +6,114 @@ del /f/s %logFolder%
 md %logFolder%
 md %logFolder%\eventlog
 
+
+
+
+@REM Netbios Name
+nbtstat -c >  %logFolder%\NetBiosCache.txt
+nbtstat -s >  %logFolder%\NetBiosSession.txt
+nbtstat -n >  %logFolder%\NetBiosName.txt
+
+copy %SystemRoot%\AppCompat\Programs\RecentFileCache.bcf %logFolder%\
+
+@rem Dump Netbios based on Current IP/Class B
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+start /B nbtscan %NetworkIP%/16 > %logFolder%\NetBios_Dump_%NetworkIP%.txt
+
+
+@rem Dump Netbios based on Current IP/Class B full
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+start /B nbtscan -f %NetworkIP%/16 > %logFolder%\NetBios_Dump_%NetworkIP%_Full.txt
+
+@REM Domain User Group
+echo --net group /domain-- > %logFolder%\NetDomainAccount.txt
+net group /domain >>  %logFolder%\NetDomainAccount.txt
+
+echo --net user /domain-- >> %logFolder%\NetDomainAccount.txt
+net user /domain >>  %logFolder%\NetDomainAccount.txt
+
+echo --net group "Domain Admins" /DOMAIN-- >> %logFolder%\NetDomainAccount.txt
+net group "Domain Admins" /DOMAIN >>  %logFolder%\NetDomainAccount.txt
+
+echo --net accounts /domain-- >> %logFolder%\NetDomainAccount.txt
+net accounts /domain > %logFolder%\NetDomainAccount.txt
+
+echo --net user-- > %logFolder%\NetAccount.txt
+net user >> %logFolder%\NetAccount.txt
+
+echo --net localgroup administrators-- >> %logFolder%\NetAccount.txt
+net localgroup administrators >> %logFolder%\NetAccount.txt
+
+echo --net accounts-- >> %logFolder%\NetAccount.txt
+net accounts >> %logFolder%\NetAccount.txt
+
+echo --net user securityadmin-- >> %logFolder%\NetAccount.txt
+net user securityadmin >> %logFolder%\NetAccount.txt
+
+@REM Dump GPO Result
+gpresult /x %logFolder%\gpresult.xml
+gpresult /h %logFolder%\gpresult.html
+gpresult /z %logFolder%\gpresultZ.txt
+
+@REM File Create Information
+dir C:\ProgramData /S /O-D /tc /q > %logFolder%\Dir_TC_ProgramData.txt
+start /B dir C:\Users /S /O-D /tc /q > %logFolder%\Dir_TC_Users.txt
+dir "C:\Program Files" /S /O-D /tc /q > %logFolder%\Dir_TC_ProgramFiles.txt
+dir "C:\Program Files (x86)" /S /O-D /tc /q > %logFolder%\Dir_TC_ProgramFilesx86.txt
+start /B dir C:\Windows /S /O-D /tc /q > %logFolder%\Dir_TC_Windows.txt
+dir C:\Temp /S /O-D /tc /q > %logFolder%\Dir_TC_Temp.txt
+start /B dir C:\ /O-D /tc /q > %logFolder%\Dir_TC_C.txt
+dir C:\ /AH /O-D /tc /q > %logFolder%\Dir_TC_Hidden.txt
+
+@REM File Modify Information
+dir C:\ProgramData /S /O-D /tw /q > %logFolder%\Dir_TW_ProgramData.txt
+start /B dir C:\Users /S /O-D /tw /q > %logFolder%\Dir_TW_Users.txt
+dir "C:\Program Files" /S /O-D /tw /q > %logFolder%\Dir_TW_ProgramFiles.txt
+dir "C:\Program Files (x86)" /S /O-D /tw /q > %logFolder%\Dir_TW_ProgramFilesx86.txt
+start /B dir C:\Windows /S /O-D /tw /q > %logFolder%\Dir_TW_Windows.txt
+dir C:\Temp /S /O-D /tw /q > %logFolder%\Dir_TW_Temp.txt
+dir C:\ /O-D /tw /q > %logFolder%\Dir_TW_C.txt
+start /B dir C:\ /AH /O-D /tw /q > %logFolder%\Dir_TW_Hidden.txt
+
+@REM System Information
+systeminfo > %logFolder%\Systeminfo.txt
+
+@rem Export Autoruns
+Autorunsc.exe /accepteula -a * -c -h -s '*' > %logFolder%\Autoruns.csv
+
+@REM Get Unsigned EXE，DLL in c:\windows
+sigcheck.exe -c -s -u c:\windows\*.exe > %logFolder%\WindowsUnsignedEXE.txt
+sigcheck.exe -c -s -u c:\windows\*.dll > %logFolder%\WindowsUnsignedDLL.txt
+
+@REM MSFT Native Backdoor Check
+echo %date% %time% > %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*sethc.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*Utilman.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*osk.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*Narrator.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*Magnify.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*AtBroker.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*explorer.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*displayswitch.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+sigcheck.exe -s c:\windows\*cmd.exe >> %logFolder%\MSFT_Native_Backdoor_Check.txt
+
+@REM Net User Account Information
+net user %UserName% /domain > %logFolder%\Account_%UserName%_Info.txt
+net localgroup "Administrators" > %logFolder%\Account_LocalAdmins_Info.txt
+
 @REM Windows Current Service
 tasklist /v > %logFolder%\tasklistVerbose.txt
 tasklist > %logFolder%\tasklistVerbose.txt
+tasklist /SVC > %logFolder%\taskSVC.txt
+
+@REM Schedule Job
+schtasks /query /v /fo CSV > %logFolder%\scheduleTasks.csv
 
 @REM DNS Cache
 ipconfig /displaydns > %logFolder%\DisplayDNS.txt
+
+@REM Web Visit Log
+Weblog /scomma %logFolder%\weblog.csv /sort "Visit Time"
 
 @REM WMIC System Information
 wmic /Output:"%logFolder%\bios.csv"  bios list full /Format:CSV
@@ -29,9 +131,11 @@ wmic /Output:"%logFolder%\nicconfig.csv"  nicconfig list full /Format:CSV
 wmic /Output:"%logFolder%\nic.csv"  nic list full /Format:CSV
 wmic /Output:"%logFolder%\netuse.csv"  netuse list full /Format:CSV
 wmic /Output:"%logFolder%\netprotocol.csv"  netprotocol list full /Format:CSV
+wmic /Output:"%logFolder%\InstalledAppList.csv"  product list full /Format:CSV
 
 @REM DNS Cache
-netstat -nba > %logFolder%NetstatAP.txt
+netstat -nba > %logFolder%\NetstatAP.txt
+netstat -fba > %logFolder%\NetstatFQDN.txt
 
 @REM GET MFT
 rawcopy64 %SYSTEMDRIVE%0 %logFolder%
@@ -47,8 +151,11 @@ rawcopy64 %SystemRoot%\system32\config\DEFAULT %logFolder%
 rawcopy64 %SystemRoot%\system32\config\SECURITY %logFolder%
 rawcopy64 %SystemRoot%\system32\config\SOFTWARE %logFolder%
 
+rawcopy64  %SystemRoot%\AppCompat\Programs\RecentFileCache.bcf %logFolder%
+
 rawcopy64 %LOCALAPPDATA%\Microsoft\Windows\UsrClass.dat %logFolder%
 
+AppCompatCacheParser.exe  -s %logFolder% -t
 
 @REM Get Event Log
 copy %SystemRoot%\System32\winevt\Logs\* %logFolder%\eventlog
